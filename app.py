93bcc19d4afb009f4d709ceedb244a4a9376d233
George Tang <gtang43@gmail.com>
2019-05-04 20:57:12 -0700

    Fixed db not defined bug

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'george'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-  db.create_all() # db will be defined by the time this function is reached
-
 jwt = JWT(app, authenticate, identity) # creates /auth
 
 api.add_resource(Item, '/item/<string:name>')
